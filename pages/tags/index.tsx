6e5575b714a3cc4dec41d734e96b95d92863684c
kgtkr <contact@kgtkr.net>
2021-11-11 17:41:56 +0900

    update font&create tag page

---
+++ pages/tags/index.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { NextPage, GetStaticProps } from "next";
+import Image from "next/image";
+import profileImage from "../public/images/profile.png";
+import { SocialIcon } from "react-social-icons";
+import Link from "next/link";
+import styles from "./Tags.module.scss";
+import matter from "gray-matter";
+
+type Props = {
+  tags: string[];
+};
+
+const Tags: NextPage<Props> = (props) => {
+  return (
+    <div className={styles.container}>
+      {props.tags.map((tag) => (
+        <div>{tag}</div>
+      ))}
+    </div>
+  );
+};
+
+export default Tags;
+
+export const getStaticProps: GetStaticProps<Props> = async () => {
+  const context = (require as any).context("../../blog", true, /\.md$/);
+  const keys: string[] = context.keys();
+
+  return {
+    props: {
+      tags: keys.flatMap((key) => {
+        const { data } = matter(context(key).default);
+        return data.tags;
+      }),
+    },
+  };
+};
