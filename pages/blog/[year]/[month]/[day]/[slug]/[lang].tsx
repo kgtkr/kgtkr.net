06fc58b9fa10e10bf2c956fcf5037dd083492943
kgtkr <contact@kgtkr.net>
2022-02-16 01:05:17 +0900

    postページのテンプレートだけ(コンパイルエラー修正用)

---
+++ pages/blog/[year]/[month]/[day]/[slug]/[lang].tsx
@@ -0,0 +1,10 @@
+import { NextPage } from "next";
+import React from "react";
+
+type Props = {};
+
+const PostWithLang: NextPage<Props> = props => {
+  return <div></div>;
+};
+
+export default PostWithLang;
---
+++ pages/blog/[year]/[month]/[day]/[slug]/index.tsx
@@ -0,0 +1,10 @@
+import { NextPage } from "next";
+import React from "react";
+
+type Props = {};
+
+const Post: NextPage<Props> = props => {
+  return <div></div>;
+};
+
+export default Post;
