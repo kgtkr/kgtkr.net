b4d37aa928bc4cb2c3e093b5c31914d0ff4775e0
kgtkr <contact@kgtkr.net>
2023-08-02 00:39:31 +0900

    viewport

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -20,6 +20,7 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Source+Code+Pro&display=swap"
           rel="stylesheet"
         />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
       <body>
         <Main />
