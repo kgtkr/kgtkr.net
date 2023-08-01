cdcaacfb3fca0080ab2ea4684027e09eb20aa9b5
kgtkr <contact@kgtkr.net>
2023-08-02 00:40:52 +0900

    fix lint

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -12,6 +12,7 @@ function App({ Component, pageProps, router }: AppProps) {
       <GoogleAnalytics />
       <Head>
         <link rel="icon" href="/images/profile.png" type="image/png" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
       <header
         className={`${styles.header} ${
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -20,7 +20,6 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Source+Code+Pro&display=swap"
           rel="stylesheet"
         />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
       <body>
         <Main />
