28d69dd0d07c43cfbd9f032c819304326255cc6e
kgtkr <me@kgtkr.net>
2025-02-11 19:04:56 +0900

    add wkd command

--- pages/about.tsx
+++ pages/about.tsx
@@ -91,6 +91,10 @@ const About: NextPage = () => {
               <a href="/B30DBE9381E03D5DF30188C81F6EB9519F573241.txt">
                 B30D BE93 81E0 3D5D F301 88C8 1F6E B951 9F57 3241
               </a>
+              <br />
+              <code>
+                $ gpg --auto-key-locate wkd --locate-external-keys me@kgtkr.net
+              </code>
             </td>
           </tr>
           <tr>
